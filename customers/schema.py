9f650149914759d3cede762182d434d73628de32
kelani34 <kelanitaiwo34@gmail.com>
2023-01-26 13:29:39 +0100

    update: change schema data

--- customers/schema.py
+++ customers/schema.py
@@ -9,9 +9,9 @@ class CustomerType(DjangoObjectType):
         fields = '__all__'
 
 class Query(graphene.ObjectType):
-    all_customers = graphene.List(CustomerType)
+    customers = graphene.List(CustomerType)
 
-    def resolve_all_customers(root, info):
+    def resolve_customers(root, info):
         return Customer.objects.all()
 
 schema = graphene.Schema(query=Query)
