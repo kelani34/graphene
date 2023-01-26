a6166fac84643ba8368f7c9cad49d8f3d4b58632
kelani34 <kelanitaiwo34@gmail.com>
2023-01-26 13:56:27 +0100

    update: add mutaions and parameters for our schema

--- customers/schema.py
+++ customers/schema.py
@@ -8,10 +8,40 @@ class CustomerType(DjangoObjectType):
         model =Customer
         fields = '__all__'
 
+class CreateCustomer(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        industry = graphene.String()
+    customer = graphene.Field(CustomerType)
+    
+    def mutate(root, info, name, industry): 
+        customer = Customer(name=name, industry=industry)
+        customer.save()
+        return CreateCustomer(customer=customer)
+
 class Query(graphene.ObjectType):
     customers = graphene.List(CustomerType)
+    customer_name_first = graphene.Field(CustomerType, name=graphene.String(required=True))
+    customer_name_multiple = graphene.List(CustomerType, name=graphene.String(required=True))
+
 
     def resolve_customers(root, info):
         return Customer.objects.all()
 
-schema = graphene.Schema(query=Query)
+    def resolve_customer_name_first(root, info, name):
+        try:
+            return Customer.objects.filter(name=name).first()
+        except Customer.DoesNotExist:
+            return None
+
+    def resolve_customer_name_multiple(root, info, name):
+        try:
+            return Customer.objects.filter(name=name)
+        except Customer.DoesNotExist:
+            return None
+
+
+class Mutations(graphene.ObjectType):
+    createCustomer = CreateCustomer.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutations)
