5f7b1c4ccc8bc2fda406c0058cebd66d4bf68341
kelani34 <kelanitaiwo34@gmail.com>
2023-01-26 01:08:26 +0100

    init

---
+++ customers/views.py
@@ -0,0 +1,60 @@
+from customers.models import Customer
+from django.http import JsonResponse, Http404
+from customers.serializers import CustomerSerializer, UserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def customers(request):
+    """
+        invoke serializer data and return it to client
+    """
+    if request.method =='GET':
+        data = Customer.objects.all()
+        serializer = CustomerSerializer(data, many=True)
+        return Response({'customers': serializer.data})
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def customer(request, id):
+    try:
+        data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(data)
+        return Response({'customer': serializer.data})
+    elif request.method =='DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def register(request):
+    serilizer = UserSerializer(data=request.data)
+    if serilizer.is_valid():
+        user= serilizer.save()
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+        return Response(tokens, status=status.HTTP_201_CREATED)
+    return Response(serilizer.errors, status=status.HTTP_400_BAD_REQUEST)
